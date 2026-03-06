92158fddc3d670d46e8a26b3fb116b7c2a0a1e0d
Zhu Jiale <Jiale.Zhu@amd.com>
2026-03-06 17:16:04 +0800

    add example

---
+++ rocm_halcyon/examples/run.py
@@ -0,0 +1,30 @@
+import rocm_halcyon as rh
+
+ops = rh.parse_torch_profiler("trace.json",device_type="amd")
+
+rh.export_to_excel(ops,
+    {
+        "gpu_kernel_name":rh.GPUKernelNameVisitor(),
+        "cpu_kernel_name":rh.CPUKernelNameVisitor(),
+        "kernel_duration":rh.KernelDurationVisitor(),
+        "stream":rh.KernelStreamIdVisitor(),
+        "start_timestamp":rh.KernelStartTimestampVisitor(),
+        "end_timestamp":rh.KernelEndTimestampVisitor(),
+        "grid":rh.KernelGridSizeVisitor(),
+        "block":rh.KernelBlockSizeVisitor(),
+        "smem":rh.KernelSharedMemorySizeVisitor(),
+        "input_shape":rh.KernelInputShapeVisitor(),
+        "input_data_type":rh.KernelInputDtypeVisitor(),
+        "input_data_size":rh.InputDataSizeVisitor(),
+        "output_data_size":rh.OutputDataSizeVisitor(),
+        "BW(GB/s)":rh.BWVsitor(),
+        "TensorCoreFloats":rh.TensorCoreFloatsVisitor(),
+        "TensorCoreTFlops":rh.TensorCoreTFlopsVisitor(),
+        "VALUFloats":rh.VALUFloatsVisitor(),
+        "VALUTFlops":rh.VALUTFloatsVisitor(),
+        "output_shape":rh.KernelOutputShapeVisitor(),
+        "output_data_type":rh.KernelOutputDtypeVisitor(),
+        "device_id":rh.KernelDeviceVisitor(),
+        "host_launching_cost":rh.KernelHostLaunchingCostVisitor()
+    },
+    file_name="data.xlsx",sheet_name="your-sheet-name")
